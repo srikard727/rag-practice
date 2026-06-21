5f94a54cb87195d894f5c5f467c26e2610b78faf
SirPopGhost <47957443+SirPopGhost@users.noreply.github.com>
2026-06-20 23:24:00 -0700

    Minor rag implentation

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
-for i in range(5):
-    print("*")
-    
+documents = [
+    "RAG means retrieval augmented generation...",
+    "Embeddings turn text into numbers...",
+    "A vector database stores embeddings..."
+]
+
+def chunk_text(text, chunk_size):
+    # return a list of smaller text chunks
