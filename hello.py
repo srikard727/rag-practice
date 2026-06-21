92ad28c7f327f72453f734e88867be9f104553d0
SirPopGhost <47957443+SirPopGhost@users.noreply.github.com>
2026-06-20 23:52:23 -0700

    Added a little more

--- hello.py
+++ hello.py
@@ -1,8 +1,19 @@
+# 1. Fake documents
 documents = [
     "RAG means retrieval augmented generation...",
     "Embeddings turn text into numbers...",
     "A vector database stores embeddings..."
 ]
 
+
+# 2. Functions
 def chunk_text(text, chunk_size):
-    # return a list of smaller text chunks
+    words = text.split()
+    print(words)
+
+# 3. Test one function
+sample_text = documents[0]
+
+chunks = chunk_text(sample_text, 5)
+
+print(chunks)
